c57f3b1365799218c403e27c26521e9629591cd9
Franck67639 <franckassontia6@gmail.com>
2026-02-03 05:55:03 +0100

    more locale settings and compiled static files

--- smart_farm/settings.py
+++ smart_farm/settings.py
@@ -150,6 +150,10 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = 'media/'
